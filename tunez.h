bd21f057155e24e9573182500defc1e378ca0bbc
eVookelj <evukelj@stuy.edu>
2016-10-13 17:55:07 -0400

    Begin tunez.h

---
+++ tunez.h
@@ -0,0 +1,26 @@
+struct song_node{
+  char name[256];
+  char artist[256];
+  struct song_node* next;
+};
+
+//print list
+void print_list(struct song_node *list);
+
+struct song_node* insert_front( struct song_node* list, char name[], char artist[] );
+
+//returns pointer to first occurence of song
+struct song_node* searchSong( struct song_node* list, char name[] );
+
+//returns pointer to first occurence of artist
+struct song_node* searchArtist( struct song_node* list, char artist[] );
+
+//return pointer to a random song
+struct song_node* randomSong( struct song_node* list );
+
+//return pointer to start of free'd list
+struct song_node* freeList( struct song_node* list);
+
+//insert notes in order??
+
+//remove single node from list??
