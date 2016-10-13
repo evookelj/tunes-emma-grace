e5117ed44931da0bb709f5bbcc165f403b5eec4a
eVookelj <evukelj@stuy.edu>
2016-10-13 18:25:58 -0400

    Uh

--- tunez.h
+++ tunez.h
@@ -4,9 +4,10 @@ struct song_node{
   struct song_node* next;
 };
 
-//print list
+//print list (DONE)
 void print_list(struct song_node *list);
 
+//returns pointer to new front of list w given attributes (DONE)
 struct song_node* insert_front( struct song_node* list, char name[], char artist[] );
 
 //returns pointer to first occurence of song
@@ -18,7 +19,7 @@ struct song_node* searchArtist( struct song_node* list, char artist[] );
 //return pointer to a random song
 struct song_node* randomSong( struct song_node* list );
 
-//return pointer to start of free'd list
+//return pointer to start of free'd list (DONE)
 struct song_node* freeList( struct song_node* list);
 
 //insert notes in order??
