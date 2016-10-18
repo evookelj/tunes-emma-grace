64064abfb5802041ba1ae99030be542ef5d2a9d6
eVookelj <evukelj@stuy.edu>
2016-10-17 20:56:32 -0400

    Start implementing (not-yet functioning) table

--- tunez.c
+++ tunez.c
@@ -5,7 +5,7 @@
 
 typedef struct song_node songNode;
 
-songNode* insert_front( songNode* list, char newName[256], char newArtist[256] ) {
+songNode* insert_front_list(songNode* list, char newName[], char newArtist[] ) {
   songNode* new = malloc(sizeof(songNode));
   strcpy(new->name,newName);
   strcpy(new->artist,newArtist);
@@ -13,13 +13,17 @@ songNode* insert_front( songNode* list, char newName[256], char newArtist[256] )
   return new;
 }
 
-songNode* insert_order( songNode* list, char newName[256], char newArtist[256] ) {
+void insertFront( songNode* table[], char newName[], char newArtist[] ) {
+  songNode* findLL = findLetterList(table,newArtist[0]);
+  findLL = insert_front_list( findLL, newName, newArtist );
+}
+
+songNode* insert_order_list( songNode* list, char newName[], char newArtist[] ) {
   songNode* new = malloc(sizeof(songNode));
   strcpy(new->name,newName);
   strcpy(new->artist,newArtist);
   songNode* curr = list;
   new->next = NULL;
-
   if(curr->next == NULL) {
     if(strcmp(newArtist, curr->artist) < 0) {
       new->next = curr;
@@ -40,16 +44,27 @@ songNode* insert_order( songNode* list, char newName[256], char newArtist[256] )
   return list;
 }
 
+void insertOrder( songNode* table[], char newName[], char newArtist[] ) {
+  songNode* findLL = findLetterList(table,newArtist[0]);
+  findLL = insert_order_list( findLL, newName, newArtist);
+}
+
 void print_list(songNode* list) {
   songNode* p = list;
   while (p != NULL) {
     printf("Song: %s, Artist: %s\n",p->name,p->artist);
     p=p->next;
   }
-  printf("NULL\n");
 }
 
-songNode* searchSong(songNode* list, char name[]) {
+void printList( songNode* table[] ) {
+  int i = 0;
+  for (i=0; i<26; i++) {
+    print_list(table[i]);
+  }
+}
+
+songNode* search_song_list(songNode* list, char name[]) {
   songNode* p = list;
   while (p != NULL) {
     if (!(strcmp(p->name,name))) { return p; }
@@ -58,7 +73,16 @@ songNode* searchSong(songNode* list, char name[]) {
   return NULL;
 }
 
-songNode* searchArtist(songNode* list, char artist[]) {
+songNode* searchSong( songNode* table[], char name[] ) {
+   int i;
+   for (i=0; i<26; i++) {
+     songNode* result = search_song_list(table[i],name);
+     if (result != NULL) { return result; }
+   }
+   return NULL;
+ }
+
+songNode* search_artist_list(songNode* list, char artist[]) {
   songNode* p = list;
   while (p != NULL) {
     if (!(strcmp(p->artist,artist))) { return p; }
@@ -67,7 +91,11 @@ songNode* searchArtist(songNode* list, char artist[]) {
   return NULL;
 }
 
-void deleteSong( songNode* list, char name[] ) {
+songNode* searchArtist( songNode* table[], char artist[] ) {
+  return search_artist_list( findLetterList(table,artist[0]),artist);
+}
+
+void delete_song_list( songNode* list, char name[] ) {
   songNode* p = list;
   while (p->next != NULL) {
     if (!(strcmp(p->next->name,name))) {
@@ -80,9 +108,19 @@ void deleteSong( songNode* list, char name[] ) {
   }
 }
 
-//THIS IS NOT WORKING YET AND IDK WHY
-//free(<songNode) is only freeing the song part of the struct
-songNode* deleteList( songNode* list ) {
+ void deleteSong( songNode* table[], char name[]) {
+   int i;
+   for (i=0; i<26; i++) {
+     songNode* result = search_song_list(table[i], name);
+     if (result != NULL) {
+       delete_song_list( table[i], name );
+       return;
+     }
+   }
+   return ;
+ }
+
+songNode* delete_list( songNode* list ) {
   songNode* p = list;
   songNode* curr;
   while(p != NULL) {
@@ -94,7 +132,16 @@ songNode* deleteList( songNode* list ) {
   return p;
 }
 
-void printArtist( songNode* list, char artist[] ){
+songNode** deleteList( songNode* table[] ) {
+   int i;
+   for (i=0; i<26; i++) {
+     delete_list(table[i]);
+   }
+   table = NULL;
+   return table;
+}
+
+void print_artist_list( songNode* list, char artist[] ){
   printf("\nSongs by %s:\n", artist);
   songNode* p = list;
   while (p != NULL) {
@@ -103,7 +150,11 @@ void printArtist( songNode* list, char artist[] ){
   }
 }
 
-void printAlpha( struct song_node* list, char x ) {
+void printArtist( songNode* table[], char artist[] ) {
+  print_artist_list( findLetterList(table,artist[0]),artist);
+ }
+
+void print_alpha_list( struct song_node* list, char x ) {
   printf("\n%c songs:\n", x);
   songNode* p = list;
   while (p != NULL) {
@@ -112,32 +163,38 @@ void printAlpha( struct song_node* list, char x ) {
   }
 }
 
-songNode* findLetterList( songNode* table[], char artist[256] ) {
-  //return table;
-  return table[(int)(artist[0]) - 97];
+ void printAlpha( songNode* table[], char x) {
+   print_alpha_list( findLetterList(table,x), x);
+ } 
+
+songNode* findLetterList( songNode* table[], char a ) {
+  return table[(int)tolower(a) - 97];
 }
 
 int main() {
   songNode* *table = calloc(26,sizeof(songNode*));
-  
   songNode* list = NULL;
   printf("TESTING ADD AND FIND:\n");
-  list = insert_front(list, "New York Cares", "Interpol");
-  list = insert_order(list, "Look At Me", "Buddy Holly");
-  list = insert_order(list, "Whole Lotta Love", "Led Zeppelin");
-  list = insert_order(list, "Mailman, Bring Me No More Blues", "Buddy Holly");
-  list = insert_order(list, "Yesterday", "The Beatles");
-  list = insert_order(list, "Black Bird", "The Beatles");
-  list = insert_order(list, "Let it be", "The Beatles");
-  print_list(list);
-  printf("Looking for 'Buddy Holly': %s\n", searchArtist(list,"Buddy Holly")->name);
-  printf("Looking for 'Whole Lotta Love': %s\n", searchSong(list, "Whole Lotta Love")->artist);
+  insertFront(table, "New York Cares", "Interpol");
+  print_list(findLetterList(table,'i'));
+  printList(table);
+  /*
+  insertOrder(table, "Look At Me", "Buddy Holly");
+  insertOrder(table, "Whole Lotta Love", "Led Zeppelin");
+  insertOrder(table, "Mailman, Bring Me No More Blues", "Buddy Holly");
+  table = insertOrder(table, "Yesterday", "The Beatles");
+  table = insertOrder(table, "Black Bird", "The Beatles");
+  table = insertOrder(table, "Let it be", "The Beatles");
+  printList(table);
+  printf("Looking for 'Buddy Holly': %s\n", searchArtist(table,"Buddy Holly")->name);
+  printf("Looking for 'Whole Lotta Love': %s\n", searchSong(table, "Whole Lotta Love")->artist);
   printf("\nTESTING DELETE of 'Black Bird':\n");
-  deleteSong(list,"Black Bird");
-  print_list(list);
+  deleteSong(table,"Black Bird");
+  printList(table);
+  */
+  deleteList(table);
 
   /*===================================
-    ====================================*/
   printf("TESTING PRINT ARTIST");
   printArtist(list, "The Beatles");
   printf("TESTING PRINT ALPHA");
@@ -146,6 +203,7 @@ int main() {
   printf("TESTING DELETE LIST:\n");
   list = deleteList(list);
   print_list(list);
+    ====================================*/
 
   return 0;
 }
--- tunez.h
+++ tunez.h
@@ -10,26 +10,42 @@ void print_list(struct song_node *list);
 //returns pointer to new front of list w given attributes (DONE)
 struct song_node* insert_front( struct song_node* list, char name[], char artist[] );
 
+//struct song_node* insertFront( struct song_node* table[], char newName[], char newArtist[] );
+
 //returns pointer to front of list w ndoe inserted in order (DONE)
 struct song_node* insert_order( struct song_node* list, char name[], char artist[] );
 
+//struct song_node* insertOrder( struct song_node* table[], char newName[], char newArtist[] );
+
 //returns pointer to first occurence of song (DONE)
-struct song_node* searchSong( struct song_node* list, char name[] );
+struct song_node* search_song_list( struct song_node* list, char name[] );
+
+//struct song_node* searchSong( struct song_node* table[], char name[] );
 
 //returns pointer to first occurence of artist (DONE)
-struct song_node* searchArtist( struct song_node* list, char artist[] );
+struct song_node* search_artist_list( struct song_node* list, char artist[] );
+
+//struct song_node* searchArtist( struct song_node* table[], char artist[] );
 
 //return pointer to start of deleted list (DONE)
-struct song_node* deleteList( struct song_node* list);
+struct song_node* delete_list( struct song_node* list);
+
+//struct song_node** deleteList( struct song_node* table[] );
 
 //delete given song name (DONE)
-void deleteSong( struct song_node* list, char name[] );
+//void deleteSong( struct song_node* list, char name[] );
 
 //print playlist in random order
-void printShuffle( struct song_node* list );
+//void printShuffle( struct song_node* list );
 
 //print all songs of artist (DONE)
-void printArtist( struct song_node* list, char artist[] );
+void print_artist_list( struct song_node* list, char artist[] );
+
+//void printArtist( struct song_node* table[], char artist[] );
 
 //print all entries under certain letter (DONE)
-void printAlpha( struct song_node* list, char x );
+void print_alpha_list( struct song_node* list, char x );
+
+//void printAlpha( struct song_node* table[], char x);
+
+struct song_node* findLetterList( struct song_node* table[], char a );
